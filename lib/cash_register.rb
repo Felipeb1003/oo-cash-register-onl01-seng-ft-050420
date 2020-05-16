3387a119ab2209e2f37b932e15830701f26e58ee
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-16 18:31:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'pry'
 class CashRegister
  attr_accessor :total, :items, :discount, :price, :quantity
@@ -36,3 +37,36 @@ class CashRegister
  end
 
 end
+=======
+class CashRegister
+
+   attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount=0)
+    @total = 0
+    @discount= discount
+    items= []
+  end  
+   
+ def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+     self.last_transaction = amount * quantity
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+  
+  end
+    
+end
+>>>>>>> ec7985bb6f2d0a4a72922d957a3e6df16d50f7a5
